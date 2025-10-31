6b15bf4c6aeb646392b0159f53379edc90364df6
ruziev-dev <ruziev.dev@gmail.com>
2025-10-31 15:48:58 +0300

    minor fix

--- libpcsclite.mk
+++ libpcsclite.mk
@@ -6,22 +6,24 @@ $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 2.4.0
 $(PKG)_SUBDIR   := pcsc-lite-$($(PKG)_VERSION)
 $(PKG)_FILE     := pcsc-lite-$($(PKG)_VERSION).tar.xz
-$(PKG)_URL      := https://pcsclite.apdu.fr/files/pcsc-lite-2.4.0.tar.xz
+$(PKG)_URL      := https://pcsclite.apdu.fr/files/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc
 
-#define $(PKG)_UPDATE
-#    $(WGET) -q -O- 'https://pcsclite.apdu.fr/files/' | \
-#    $(SED) -n 's,.*pcsc-lite-\([0-9.]*\)\.tar.*,\1,p' | \
-#    tail -1
-#endef
+#wget -P /opt/mxe/pkg/ https://pcsclite.apdu.fr/files/pcsc-lite-2.4.0.tar.xz
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'https://pcsclite.apdu.fr/files/' | \
+    $(SED) -n 's,.*pcsc-lite-\([0-9.]*\)\.tar.*,\1,p' | \
+    tail -1
+endef
 
 define $(PKG)_BUILD
-    cd '$(1)' && ./configure \
+     cd '$(BUILD_DIR)' && '$(SOURCE_DIR)/configure' \
         $(MXE_CONFIGURE_OPTS) \
-        --disable-libsystemd \
-        --enable-static \
-        --disable-shared
-    $(MAKE) -C '$(1)' -j '$(JOBS)' \
-        CFLAGS='-DWIN32'
-    $(MAKE) -C '$(1)' -j 1 install
+        --disable-gtk-doc \
+        --disable-man \
+        --without-tools \
+        --without-imagemagick
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' LDFLAGS='-no-undefined'
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install
 endef
